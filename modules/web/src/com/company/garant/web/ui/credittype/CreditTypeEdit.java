da5ab82330f9c7aeca7e48054286b08c26841115
v.galkov <v.galkov@argustelecom.ru>
2024-11-18 17:52:37 +0400

    4.4
    доделать
    1. UniqueNumbersService
    2. Фильтр bankFilter не виден или что.

--- modules/web/src/com/company/garant/web/ui/credittype/CreditTypeEdit.java
+++ modules/web/src/com/company/garant/web/ui/credittype/CreditTypeEdit.java
@@ -6,12 +6,29 @@
 
 package com.company.garant.web.ui.credittype;
 
+import com.haulmont.cuba.core.app.UniqueNumbersService;
+import com.haulmont.cuba.gui.components.TextField;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.garant.entity.CreditType;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @UiController("garant$CreditType.edit")
 @UiDescriptor("credit-type-edit.xml")
 @EditedEntityContainer("creditTypeDc")
 @LoadDataBeforeShow
 public class CreditTypeEdit extends StandardEditor<CreditType> {
+    @Autowired
+    private TextField<String> codeField;
+    @Autowired
+    private UniqueNumbersService uniqueNumbersService;
+
+    @Subscribe
+    public void onInitEntity(InitEntityEvent<CreditType> event) {
+        //TODO сделать счётчик.
+        String value = String.valueOf(uniqueNumbersService.getNextNumber("zzzz?"));
+        codeField.setValue(value);
+        getEditedEntity().setCode(value);
+    }
+
+
 }
