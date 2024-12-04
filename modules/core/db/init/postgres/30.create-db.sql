7a2e573ebe38f0008dac0b0725f420a00bfa83e3
v.galkov <v.galkov@argustelecom.ru>
2024-12-04 15:07:57 +0400

    9 набиты данные, нужные для работы проекта.
    WF_DEFAULT_PROC_ACTOR - заполнить данными вместо ручной правки.
    
    ====================
    2. недоделанная болванка -
    2.1. приделать какие-то скрипты для СБ к дизайну процесса,
    2.2. какую-то блокировку на погашение кредита по 'repaymentAmountField' полю с суммой Погашение кредита.

--- modules/core/db/init/postgres/30.create-db.sql
+++ modules/core/db/init/postgres/30.create-db.sql
@@ -58,10 +58,10 @@ VALUES(newid(), 1, now(), 'admin', now(), NULL, NULL, NULL, 'goal 3', '3');
 ^
 
 insert into sec_role(id, create_ts, created_by, version, name, loc_name, description, role_type, is_default_role, dtype) values
-(newId(), now(), 'system', 1,'Manager', 'Менеджер', 'Менеджер', 0, false, '10');
+(newId(), now(), 'system', 1,'Менеджер', 'Менеджер', 'Менеджер', 0, false, '10');
 
 insert into sec_role(id, create_ts, created_by, version, name, loc_name,description, role_type, is_default_role, dtype) values
-(newId(), now(), 'system', 1,'Operator', 'Оператор', 'Оператор', 0, false, '10');
+(newId(), now(), 'system', 1,'Оператор', 'Оператор', 'Оператор', 0, false, '10');
 
 insert into sec_role(id, create_ts, created_by, version, name, loc_name,description, role_type, is_default_role, dtype) values
 (newId(), now(), 'system', 1,'securityDepartment', 'СБ', 'СБ', 0, false, '10');
